1597103424a0b7579c5771f667af19e515f89aee
Samir <cerbekovsamir@gmail.com>
2025-04-14 10:35:11 +0500

    kbfwuid

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { DonateComponent } from './donate/donate.component';
 import { ServersComponent } from './servers/servers.component';
 import { InfoComponent } from './info/info.component';
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent },
   { path: 'donate', component: DonateComponent },
   { path: 'servers', component: ServersComponent },
-  { path: 'info', component: InfoComponent }
+  { path: 'info', component: InfoComponent },
+  // Add a default route or redirect if desired
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // optional
 ];
 
 @NgModule({
@@ -15,4 +19,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-
